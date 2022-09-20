97c61f6f28ae1b2683b7a6a7cbd7cb68a061c4fd
Jason Loveman <jloveman@gmail.com>
2022-09-19 23:29:55 -0700

    add the login functionality, using a fixed salt so that mongo can compare add the files array to user records add the dlusers array to files records

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ import bb from 'express-busboy';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import {MongoClient, ServerApiVersion} from 'mongodb';
-import bcrypt from 'bcrypt';
 import g from './globals.js';
 
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
+
+import {renderFile as ejsRender} from 'ejs';
 
 var app = express();
 
@@ -21,6 +22,11 @@ const __dirname = dirname(__filename);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.engine('html', ejsRender);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'public'));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
@@ -44,27 +50,23 @@ bb.extend(app, {
 // mongodb
 const mongodb = new MongoClient(g.dburi, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-export let db;
-export let users;
-export let files;
-
 async function main() {
     // Use connect method to connect to the server
     await mongodb.connect();
     console.log('Connected successfully to server');
-    db = mongodb.db(g.dbName);
-    users = db.collection('users');
-    files = db.collection('files');
-    // first time add user 'admin' pass: 'gallifrey'
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(g.defpass, salt);
+    const db = mongodb.db(g.dbName);
+    g.users = db.collection('users');
+    g.files = db.collection('files');
+    // g.salt = await bcrypt.genSalt(10);
+    const hash = await g.hashit(g.defpass);
 // upsert to the users collection
-    const doc = await users.findOneAndUpdate(
+    await g.users.findOneAndUpdate(
         {name: g.defname},
         {$set: {
                 hashpass: hash,
                 verified: true,
-                upload: true
+                upload: true,
+                downloads: []
             }},
         {upsert: true, returnNewDocument: true}
     );
@@ -72,11 +74,10 @@ async function main() {
     return 'done.';
 }
 
-
-app.listen(3000, () => {
-    main().then(() => {
-        console.log("server is up");
-    });
+main().then(() => {
+        app.listen(3000, () => {
+            console.log("http server up on port 3000");
+        })
 });
 
 // export default app;
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,43 @@
 import express from 'express';
-import { users } from '../app.js';
 import g from '../globals.js';
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   if (req.session.user) {
-    res.render('index', {title: 'Express'});
-  }
-  else {
+    let p = req.query.newfiles ?
+        // g.files.find({_id: {$nin: req.session.user.downloads}}) :
+        g.files.find({dlusers: { $in: [ req.session.user._id]}}):
+        g.files.find();
+    p.then((finds) => {
+      res.render('library', {files: finds});
+    });
+  } else {
     res.render('login');
   }
+})
+
+render.get('/upload', (req,res) => {
+  res.render('upload');
+});
+
+render.post('/upload', (req, res) => {
 });
 
 router.post('/login', (req,res) => {
   let user = req.body.user;
   let pass = req.body.pass;
-  let hashpass = g.hashit(pass);
-  users.findOne({name: user, hashpass: hashpass}).then((found) => {
-    req.session.user = found;
-    res.redirect('/');
+  g.hashit(pass).then((hashpass) => {
+    g.users.findOne({name: user, hashpass: hashpass}).then((found) => {
+      if (found) {
+        req.session.user = found;
+        res.redirect('/');
+      } else {
+        res.render('login', {fail: true})
+      }
+    })
   })
-  res.render('login', {fail: true})
 });
 
 export default router;
